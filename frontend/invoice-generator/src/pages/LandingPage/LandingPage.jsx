1576b5f5357baac99db28f8270d33b31a1c07460
Utsav Patel <158522755+utsavpatel562@users.noreply.github.com>
2025-10-27 09:43:34 -0400

    latest commit

--- frontend/invoice-generator/src/pages/LandingPage/LandingPage.jsx
+++ frontend/invoice-generator/src/pages/LandingPage/LandingPage.jsx
@@ -1,3 +1,4 @@
+import Features from "../../components/landing/Features";
 import Headers from "../../components/landing/Headers";
 import Hero from "../../components/landing/Hero";
 
@@ -5,8 +6,9 @@ const LandingPage = () => {
   return (
     <div className="bg-white text-gray-600">
       <Headers />
-      <main>
+      <main className="mb-[100vh]">
         <Hero />
+        <Features />
       </main>
     </div>
   );
---
+++ frontend/invoice-generator/src/utils/data.js
@@ -0,0 +1,24 @@
+import { BarChart2, FileText, Mail, Sparkles } from "lucide-react";
+
+export const FEATURES = [
+    {
+        icon: Sparkles,
+        title: "AI Invoice Creation",
+        description: "Paste any text, email or receipt, and let our AI instantly generate a complete structured invoice, ready to send or export."
+    },
+    {
+        icon: BarChart2,
+        title: "AI Powered Dashboard",
+        description: "Get smart, actionable insights about your business finances, generated automatically from every transaction in real time with precision."
+    },
+    {
+        icon: Mail,
+        title: "Smart Reminders",
+        description: "Automatically generate polite and effective payment reminder emails for overdue payments, keeping your cash flow on track."
+    },
+    {
+        icon: FileText,
+        title: "Invoice Management",
+        description: "Easily manage all your invoices, track payments, and send reminders for overdue payments - all from one simple dashboard."
+    },
+]
