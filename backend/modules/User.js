3918847470ea10fd3726969d3bb19bd4f462ed1a
Utsav Patel <158522755+utsavpatel562@users.noreply.github.com>
2025-10-28 10:50:46 -0400

    latest commit

---
+++ backend/modules/User.js
@@ -0,0 +1,40 @@
+// modules/User.js
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema (
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowerCase: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 6,
+            select: false,
+        },
+        businessName: {type: String, default: ''},
+        address: {type: String, default: ''},
+        phone: {type: String, default: ''},
+    },
+    {timestamps: true}
+)
+// password hashing middleware
+userSchema.pre("save", async function(next) {
+    if(!this.isModified("password")) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+})
+// method to check password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+module.exports = mongoose.model("User", userSchema);  
