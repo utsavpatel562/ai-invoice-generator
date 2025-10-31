f4e1debd85c1c759a845bc3d4b664d7d059bee4e
Utsav Patel <158522755+utsavpatel562@users.noreply.github.com>
2025-10-31 15:04:39 -0400

    latest commit

--- backend/controllers/invoiceController.js
+++ backend/controllers/invoiceController.js
@@ -4,7 +4,41 @@ const Invoice = require("../modules/Invoice");
 // @access   Private
 exports.createInvoice = async(req, res) => {
     try {
+        const user = req.user;
+        const {
+            invoiceNumber,
+            invoiceDate,
+            dueDate,
+            billFrom,
+            billTo,
+            items,
+            notes,
+            paymentTerms,
+        } = req.body;
 
+        // subtotal calculation
+        let subtotal = 0;
+        let taxTotal = 0;
+        items.forEach((item) => {
+            subtotal += item.unitPrice * item.quantity;
+            taxTotal += ((item.unitPrice * item.quantity) * (item.taxPercent || 0)) / 100;
+        })
+        const total = subtotal + taxTotal;
+        const invoice = new Invoice ({
+            invoiceNumber,
+            invoiceDate,
+            dueDate,
+            billFrom,
+            billTo,
+            items,
+            notes,
+            paymentTerms,
+            subtotal,
+            taxTotal,
+            total
+        })
+        await invoice.save();
+        res.status(201).json(invoice);
     } catch(error) {
         res.status(500).json({message: "Error creating invoice", error: error.message});
     }
@@ -13,19 +47,43 @@ exports.createInvoice = async(req, res) => {
 // @desc     Get all invoices of logged-in user
 // @route    POST /api/invoices
 // @access   Private
-exports.getInvoices = async(req, res) => {}
+exports.getInvoices = async(req, res) => {
+     try {
+
+    } catch(error) {
+        res.status(500).json({message: "Error creating invoice", error: error.message});
+    }
+}
 
 // @desc     Get single invoice by ID
 // @route    POST /api/invoices
 // @access   Private
-exports.getInvoiceById = async(req, res) => {}
+exports.getInvoiceById = async(req, res) => {
+     try {
+
+    } catch(error) {
+        res.status(500).json({message: "Error creating invoice", error: error.message});
+    }
+}
 
 // @desc     Update invoice
 // @route    POST /api/invoices
 // @access   Private
-exports.updateInvoice = async(req, res) => {}
+exports.updateInvoice = async(req, res) => {
+     try {
+
+    } catch(error) {
+        res.status(500).json({message: "Error creating invoice", error: error.message});
+    }
+}
 
 // @desc     Delete invoice
 // @route    POST /api/invoices
 // @access   Private
-exports.deleteInvoice = async(req, res) => {}
+exports.deleteInvoice = async(req, res) => {
+     try {
+
+    } catch(error) {
+        res.status(500).json({message: "Error creating invoice", error: error.message});
+    }
+}
