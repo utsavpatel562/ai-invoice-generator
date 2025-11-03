c357a577bb115783487d5a697ed06a3a5983ed31
Utsav Patel <158522755+utsavpatel562@users.noreply.github.com>
2025-11-03 11:17:43 -0500

    latest commit

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const path = require("path");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes"); 
 const invoiceRoutes = require("./routes/invoiceRoutes"); 
+const aiRoutes = require("./routes/aiRoutes"); 
 const app = express ();
 
 // middleware to handle CORS 
@@ -23,6 +24,7 @@ app.use(express.json());
 // Routes Here
 app.use("/api/auth", authRoutes);
 app.use("/api/invoices", invoiceRoutes);
+app.use("/api/ai", aiRoutes);
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
