4e694463a5e9c6b61ae22a848b39f2016f4efa45
Utsav Patel <158522755+utsavpatel562@users.noreply.github.com>
2025-11-10 09:31:20 -0500

    latest commit

---
+++ frontend/invoice-generator/src/context/AuthContext.jsx
@@ -0,0 +1,40 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const AuthContext = createContext();
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    checkAuthStatus();
+  }, []);
+
+  const checkAuthStatus = async () => {};
+
+  const login = (userData, token) => {};
+
+  const logout = () => {};
+
+  const updateUser = (updatedUserData) => {};
+
+  const value = {
+    user,
+    loading,
+    isAuthenticated,
+    login,
+    logout,
+    updateUser,
+    checkAuthStatus,
+  };
+
+  return <AuthContext.Provider>{children}</AuthContext.Provider>;
+};
